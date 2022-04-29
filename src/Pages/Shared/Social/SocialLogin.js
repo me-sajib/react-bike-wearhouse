d3eb101314446cf0004b6e5674127733a173cd37
sajib <me.mrsajib@gmail.com>
2022-04-29 23:13:12 +0600

    add social button

--- src/Pages/Shared/Social/SocialLogin.js
+++ src/Pages/Shared/Social/SocialLogin.js
@@ -3,12 +3,32 @@ import "./css/SocialLogin.css";
 
 const SocialLogin = () => {
   return (
-    <div className="text-light">
+    <div className="text-light pt-5">
       <div className="container">
+        {/* or text top of the social button */}
         <div className="d-flex">
-          <div className="hr-row"></div>
+          <div className="hr-row mt-3"></div>
           <div className="mx-2">or</div>
-          <div className="hr-row"></div>
+          <div className="hr-row mt-3"></div>
+        </div>
+
+        {/* social button */}
+        <div className="d-flex py-5 justify-content-center">
+          <div className="mx-2">
+            <button className="btn btn-primary">
+              <i className="fa-brands fa-google"></i>
+            </button>
+          </div>
+          <div className="mx-2">
+            <button className="btn btn-info">
+              <i className="fa-brands fa-github"></i>
+            </button>
+          </div>
+          <div className="mx-2">
+            <button className="btn btn-success">
+              <i className="fa-brands fa-facebook-square"></i>
+            </button>
+          </div>
         </div>
       </div>
     </div>
