e16e91aae10b3cf242e0f018403bb0f486f0ddcd
sajib <me.mrsajib@gmail.com>
2022-04-29 22:18:11 +0600

    social page add

---
+++ src/Pages/Shared/Social/SocialLogin.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./css/SocialLogin.css";
+
+const SocialLogin = () => {
+  return (
+    <div className="text-light">
+      <div className="container">
+        <div className="d-flex">
+          <div className="hr-row"></div>
+          <div className="mx-2">or</div>
+          <div className="hr-row"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SocialLogin;
