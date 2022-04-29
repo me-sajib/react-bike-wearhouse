46678c5032dc0788dadc8e3eb237276b7a62d5cb
sajib <me.mrsajib@gmail.com>
2022-04-29 12:39:07 +0600

    add spinner pages and show in items

--- src/Pages/Home/Items.js
+++ src/Pages/Home/Items.js
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from "react";
+import Spinner from "../Shared/Spinner";
 import Item from "./Item";
 
 const Items = () => {
   const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   // load data from API
   useEffect(() => {
     fetch("http://localhost:5000/inventory")
       .then((res) => res.json())
-      .then((data) => setItems(data));
+      .then((data) => {
+        setItems(data);
+      });
+    setLoading(false);
   }, []);
   return (
     <div className="items bg-dark py-5">
@@ -21,6 +26,8 @@ const Items = () => {
 
         {/* show the all items */}
         <div class="row row-cols-1 row-cols-md-2 g-4">
+          {/* spinner show */}
+          {loading && <Spinner />}
           {items.map((item) => (
             <Item key={item._id} item={item} />
           ))}
