a99b3f678fa488a044610e742153edc21150745f
sajib <me.mrsajib@gmail.com>
2022-04-29 17:41:44 +0600

    toastify header add

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Toaster } from "react-hot-toast";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home/Home";
@@ -13,6 +14,7 @@ function App() {
         <Route path="/checkItem/:id" element={<CheckItem />} />
         <Route path="*" element={<div>Not Found</div>} />
       </Routes>
+      <Toaster />
     </div>
   );
 }
