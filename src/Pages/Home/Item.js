5235ea857f10be3a435cfcc563a14f8606fbd58d
sajib <me.mrsajib@gmail.com>
2022-04-30 13:24:32 +0600

    items title text bold style add

--- src/Pages/Home/Item.js
+++ src/Pages/Home/Item.js
@@ -1,25 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./css/Item.css";
 
 const Item = ({ item }) => {
   const { _id, name, image, supplier, price, description, quantity } = item;
 
   return (
-    <div className="col">
+    <div className="col text-light">
       <div className="card">
         <img src={image} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{name}</h5>
           <p className="card-text">{description}</p>
           <p className="card-text">
-            price:
+            <b> price:</b>
             <i> {price}</i>
           </p>
           <p className="card-text">
-            supplier:
+            <b> supplier:</b>
             <i> {supplier}</i>
           </p>
-          <p className="cart-text">Quantity: {quantity}</p>
+          <p className="cart-text">
+            <b>Quantity:</b> {quantity}
+          </p>
           <Link to={`/checkItem/${_id}`} className="btn btn-primary">
             UPDATE
           </Link>
