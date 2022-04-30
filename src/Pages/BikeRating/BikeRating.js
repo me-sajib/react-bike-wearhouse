cf58a06b3bb36d26cd4408be2fabc60e0e978b1e
sajib <me.mrsajib@gmail.com>
2022-04-30 13:19:57 +0600

    add to bike rating pages

---
+++ src/Pages/BikeRating/BikeRating.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./css/BikeRating.css";
+const BikeRating = () => {
+  return (
+    <div className="bike-rating ">
+      <div className="bg-text">
+        <div className="container">
+          <h2>Our best bike</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BikeRating;
