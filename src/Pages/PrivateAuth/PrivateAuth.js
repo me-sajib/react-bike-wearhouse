4fd115c566ecc16b8004548d24563ff724c75f07
sajib <me.mrsajib@gmail.com>
2022-04-30 10:44:09 +0600

    private route auth set

--- src/Pages/PrivateAuth/PrivateAuth.js
+++ src/Pages/PrivateAuth/PrivateAuth.js
@@ -1,16 +1,11 @@
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Navigate, useLocation } from "react-router-dom";
+import auth from "../../firebase.init";
 
 function PrivateAuth({ children }) {
-  let auth = useAuthState();
+  let [user] = useAuthState(auth);
   let location = useLocation();
-  // let from = location.state?.from?.pathname || "/";
-  //   navigate(from, { replace: true });
-  if (!auth.user) {
-    // Redirect them to the /login page, but save the current location they were
-    // trying to go to when they were redirected. This allows us to send them
-    // along to that page after they login, which is a nicer user experience
-    // than dropping them off on the home page.
+  if (!user) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
