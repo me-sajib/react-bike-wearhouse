ed657e0b38ad7ff69e379059e7a05156c990a20f
sajib <me.mrsajib@gmail.com>
2022-05-01 16:18:47 +0600

    text change

--- src/Pages/UserItem/UserItems.js
+++ src/Pages/UserItem/UserItems.js
@@ -4,6 +4,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
 import Item from "../Home/Item";
 import Spinner from "../Shared/Spinner";
+import "./css/UserItem.css";
 
 const MyItem = () => {
   const [items, setItems] = useState([]);
@@ -29,19 +30,24 @@ const MyItem = () => {
     }
     getItem();
     setLoading(false);
-  }, []);
+  }, [loading]);
 
   return (
-    <div className="bg-dark my-item py-5">
+    <div className="bg-dark my-item py-5 light-border">
       <div className="container">
-        <h1 className="text-center text-primary">
-          YOU ADDED <b className="text-light">{items.length}</b> ITEM
-        </h1>
+        {items.length > 0 ? (
+          <h1 className="text-center text-primary">
+            YOU ADDED <b className="text-light">{items.length}</b> ITEM
+          </h1>
+        ) : (
+          <h1 className="no-item text-center py-3 text-danger">0</h1>
+        )}
+
         <div className="row">
           {loading && <Spinner />}
           {items.length < 1 && (
             <h3 className="text-center text-danger py-5">
-              You have no item added
+              You have not added any items
             </h3>
           )}
           <div className="row row-cols-1 my-5 row-cols-md-2 g-4">
