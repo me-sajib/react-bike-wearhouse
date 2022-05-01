d50d243b605cfdf107e1dff68d7cccb804151f56
sajib <me.mrsajib@gmail.com>
2022-05-01 12:10:16 +0600

    change to text

--- src/Pages/BikeRating/BikeRating.js
+++ src/Pages/BikeRating/BikeRating.js
@@ -4,8 +4,33 @@ const BikeRating = () => {
   return (
     <div className="bike-rating ">
       <div className="bg-text">
-        <div className="container">
-          <h2>Our best bike</h2>
+        <div className="container text-white">
+          <div className="row row-cols-1 my-5 row-cols-md-3 g-3">
+            <div className="col">
+              <div className="">
+                <h2>
+                  <i class="fa-solid fa-user"></i>
+                </h2>
+                <h1>30+</h1>
+              </div>
+            </div>
+            <div className="col">
+              <div className="">
+                <h2>
+                  <i class="fa-solid fa-users"></i>
+                </h2>
+                <h1>9000+</h1>
+              </div>
+            </div>
+            <div className="col">
+              <div className="">
+                <h1>
+                  <i class="fa-solid fa-motorcycle"></i>
+                </h1>
+                <h2>300+</h2>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
