f82d79707b8f048466df3c458ff1086e7cbc8e0d
sajib <me.mrsajib@gmail.com>
2022-05-02 17:55:47 +0600

    service details add to bike rating

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import BikeRating from "../BikeRating/BikeRating";
+import ServiceDetails from "../ServiceDetails/ServiceDetails";
 import TopSeller from "../TopSeller/TopSeller";
 import Banner from "./Banner";
 import Items from "./Items";
@@ -9,7 +9,7 @@ const Home = () => {
     <div>
       <Banner />
       <Items />
-      <BikeRating />
+      <ServiceDetails />
       <TopSeller />
     </div>
   );
