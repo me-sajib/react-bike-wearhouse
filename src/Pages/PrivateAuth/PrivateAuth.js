dd650f941838d38ce5c8b86bf6894a09192025ea
sajib <me.mrsajib@gmail.com>
2022-05-06 11:44:07 +0600

    user check and redirect to same page

--- src/Pages/PrivateAuth/PrivateAuth.js
+++ src/Pages/PrivateAuth/PrivateAuth.js
@@ -1,10 +1,17 @@
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Navigate, useLocation } from "react-router-dom";
 import auth from "../../firebase.init";
+import Spinner from "../Shared/Spinner";
 
 function PrivateAuth({ children }) {
-  let [user] = useAuthState(auth);
+  let [user, loading] = useAuthState(auth);
   let location = useLocation();
+  if (loading) {
+    return <Spinner />;
+  }
+  if (user) {
+    return children;
+  }
   if (!user) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
