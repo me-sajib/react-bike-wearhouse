852e77c71d902a444dd71f4f625a7295ed36b232
sajib <me.mrsajib@gmail.com>
2022-05-07 20:31:47 +0600

    our team page add

---
+++ src/Pages/Team/Team.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Team = () => {
+  return (
+    <div className="bg-dark light-border">
+      <div className="container py-5">
+        <h1 className="text-center py-3">Team</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Team;
